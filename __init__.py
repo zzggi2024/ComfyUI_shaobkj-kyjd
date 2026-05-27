6a82870cf8ef4bec0f60b66f3b0b84b8caf4ea08
Shaobkj Publisher <shaobkj-publisher@local>
2026-05-27 23:19:46 +0800

    update ComfyUI_shaobkj-kyjd release

--- __init__.py
+++ __init__.py
@@ -102,5 +102,5 @@ WEB_DIRECTORY = "js"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY", "__version__"]
 
 
-WEB_DIRECTORY = "js"
+WEB_DIRECTORY = "./web"
 _wrap_node_mappings(globals())
